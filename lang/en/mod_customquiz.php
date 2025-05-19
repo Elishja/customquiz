42b6935b031644da6a134788f93da95f91ff844c
Ingrid Vladimisky <ingrid.vladimisky@northius.com>
2025-05-19 10:36:17 +0200

    Test Nombre

---
+++ lang/en/mod_customquiz.php
@@ -0,0 +1,44 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'Custom quiz';
+$string['modulenameplural'] = 'Custom quizzes';
+$string['modulename_help'] = 'Allows students to practice questions selected from question bank categories.';
+$string['pluginname'] = 'Custom quiz';
+
+$string['customquizname'] = 'Quiz name';
+$string['customquizname_help'] = 'Enter the name that will appear for the activity in the course.';
+
+$string['customquizsettings'] = 'Quiz settings';
+$string['customquizfieldset'] = 'Custom quiz options';
+
+$string['allowedcategories'] = 'Available categories for students';
+$string['minquestions'] = 'Minimum number of questions';
+$string['maxquestions'] = 'Maximum number of questions';
+$string['timeperquestion'] = 'Time per question (seconds)';
+$string['onlyunanswered'] = 'Only unanswered questions';
+$string['onlyincorrect'] = 'Only incorrectly answered questions';
+$string['attemptbehaviour'] = 'Attempt behaviour';
+
+$string['score'] = 'Score';
+$string['correctanswers'] = 'Correct questions';
+$string['totalquestions'] = 'Total questions';
+$string['status'] = 'Status';
+$string['attemptdate'] = 'Attempt date';
+$string['reviewattempt'] = 'Review attempt';
+
--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_customquiz';
 $plugin->release = '0.1.0';
-$plugin->version = 2025051601;
+$plugin->version = 2025051603;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
